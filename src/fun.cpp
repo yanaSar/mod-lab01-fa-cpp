d6f42955566ad61ee83b40fbe605208896fcc96a
yanaSar <Sarajkina.Yana@yandex.ru>
2026-03-29 16:43:01 +0300

    task1

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,108 @@
-// Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <cctype>
+#include <cmath>
+
+bool isLetter(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    if (str == nullptr) return 0;
+    
+    unsigned int count = 0;
+    bool inWord = false;
+    bool hasDigit = false;
+    
+    for (const char *p = str; *p != '\0'; ++p) {
+        if (isLetter(*p) || *p == '\'') {
+            if (!inWord) {
+                inWord = true;
+                hasDigit = false;
+            }
+        } else if (std::isdigit(static_cast<unsigned char>(*p))) {
+            if (inWord) hasDigit = true;
+        } else {
+            if (inWord && !hasDigit) count++;
+
+            inWord = false;
+            hasDigit = false;
+        }
+    }
+    
+    if (inWord && !hasDigit) count++;
+    
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    if (str == nullptr) return 0;
+    
+    unsigned int count = 0;
+    bool inWord = false;
+    bool validWord = true;
+    bool firstChar = true;
+    
+    for (const char *p = str; *p != '\0'; ++p) {
+        if (isLetter(*p)) {
+            if (!inWord) {
+                inWord = true;
+                firstChar = true;
+                validWord = true;
+            }
+            
+            if (firstChar) {
+                if (!std::isupper(static_cast<unsigned char>(*p))) validWord = false;
+
+                firstChar = false;
+            } else {
+                if (!std::islower(static_cast<unsigned char>(*p))) validWord = false;
+            }
+        } else if (*p == ' ') {
+            if (inWord && validWord) count++;
+
+            inWord = false;
+        } else {
+            if (inWord) validWord = false;
+        }
+    }
+    
+    if (inWord && validWord) count++;
+    
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    if (str == nullptr) return 0;
+    
+    unsigned int totalLength = 0;
+    unsigned int wordCount = 0;
+    bool inWord = false;
+    unsigned int currentLength = 0;
+    
+    for (const char *p = str; *p != '\0'; ++p) {
+        if (isLetter(*p)) {
+            if (!inWord) {
+                inWord = true;
+                currentLength = 1;
+            } else {
+                currentLength++;
+            }
+        } else {
+            if (inWord) {
+                totalLength += currentLength;
+                wordCount++;
+                inWord = false;
+            }
+        }
+    }
+    
+    if (inWord) {
+        totalLength += currentLength;
+        wordCount++;
+    }
+    
+    if (wordCount == 0) return 0;
+    
+    double average = static_cast<double>(totalLength) / wordCount;
+    return static_cast<unsigned int>(std::round(average));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
-// Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
+    //for #1
+    const char *test1 = "Hello world 123 tes3 abc42 def";
+    std::cout << "Test 1: " << faStr1(test1) << std::endl; //3
+
+    //for #2
+    const char *test2 = "Hello world test Xyz123 Good";
+    std::cout << "Test 2: " << faStr2(test2) << std::endl; //2
+
+    //for #3
+    const char *test3 = "Hello world from C++ programming";
+    std::cout << "Test 3: " << faStr3(test3) << std::endl; //5
+
     return 0;
 }
